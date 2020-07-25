e332273ee581c2b35c5c91ecac16ae7a9ce7dd59
Jodi Hartman <chuthiminhhang_t62@hus.edu.vn>
2020-07-25 17:47:27 +0700

    fix xml

--- assets/js/main1.js
+++ assets/js/main1.js
@@ -4,8 +4,8 @@ function openCvReady() {
     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     .then(function(stream) {
         video.srcObject = stream;
-        video.play();
-    })
+        video.play()
+;    })
     .catch(function(err) {
         console.log("An error occurred! " + err);
     });
@@ -20,24 +20,29 @@ function openCvReady() {
     utils.createFileFromUrl(faceCascadeFile, faceCascadeFile, () => {
     classifier.load(faceCascadeFile); // in the callback, load the cascade from file 
 });
+
     const FPS = 24;
     function processVideo() {
         let begin = Date.now();
+
         cap.read(src);
         src.copyTo(dst);
         cv.cvtColor(dst, gray, cv.COLOR_RGBA2GRAY, 0);
+
         try{
             classifier.detectMultiScale(gray, faces, 1.1, 3, 0);
             console.log(faces.size());
         }catch(err){
             console.log(err);
         }
+
         for (let i = 0; i < faces.size(); ++i) {
             let face = faces.get(i);
             let point1 = new cv.Point(face.x, face.y);
             let point2 = new cv.Point(face.x + face.width, face.y + face.height);
             cv.rectangle(dst, point1, point2, [255, 0, 0, 255]);
         }
+
         cv.imshow("canvas_output", dst);
         // schedule next one.
         let delay = 1000/FPS - (Date.now() - begin);
